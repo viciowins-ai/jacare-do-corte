9fb101ae54c5c389b835725034ca1488296c201b
Humberto Miranda <humberto@jacaredocorte.com>
2026-01-12 15:03:16 -0300

    fix(pwa): add purpose maskable to icons and set lang to pt-BR

--- vite.config.ts
+++ vite.config.ts
@@ -18,16 +18,19 @@ export default defineConfig({
         background_color: '#ffffff',
         display: 'standalone',
         orientation: 'portrait',
+        lang: 'pt-BR',
         icons: [
           {
             src: '/icon-192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           },
           {
             src: '/icon-512.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       }
